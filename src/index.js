f4e4c5973f5154d99230db3a8de827106bb6344b
Set013 <andreykarnilovi4@gmail.com>
2023-02-05 20:07:01 +0100

    feat: human ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function humanize(num) {
+    var ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    var tens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    if ((num >= 100) & (num < 1000)) {
+        return (
+            ones[Math.floor(num / 100)] +
+            " hundred" +
+            (num % 100 ? " " + humanize(num % 100) : "")
+        );
+    }
+    if ((num >= 20) & (num < 100)) {
+        return (
+            tens[Math.floor(num / 10) - 2] +
+            (num % 10 ? " " + humanize(num % 10) : "")
+        );
+    }
+    if ((num >= 0) & (num < 20)) {
+        return ones[num];
+    }
+};
